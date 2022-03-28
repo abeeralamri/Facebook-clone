a709b74bf9b6c020b68db7860e3e697ee6039a08
abeeralamri <abeer.alamri1997@gmail.com>
2022-03-28 16:55:15 +0300

    Model Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_27_094343) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_28_134701) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,17 +39,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_094343) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "first_names", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "last_name"
+  create_table "posts", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_first_names_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_first_names_on_reset_password_token", unique: true
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -57,7 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_094343) do
     t.string "job"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -77,5 +72,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_094343) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "profiles", "users"
 end
