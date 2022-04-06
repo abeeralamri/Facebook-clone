2b82b9e96d6da2ec1f317b0124a26e50389814a2
fatenalsahli <97634511+fatenalsahli@users.noreply.github.com>
2022-04-06 19:49:56 +0300

    Some changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
       def show
         @user = User.find_by_id(params[:id])
       end
-      
       def destroy
         @user = User.find(params[:id])
         @user.destroy
